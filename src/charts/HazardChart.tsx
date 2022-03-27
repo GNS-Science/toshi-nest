fc4759cb4abe5b6681c55f0d8d9482ea08827a0c
QY <linqianye@tutanota.com>
2022-03-28 11:30:21 +1300

    add hazard chart components

---
+++ src/charts/HazardChart.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface HazardChartProps {
+  id: string;
+}
+
+const HazardChart: React.FC<HazardChartProps> = ({ id }: HazardChartProps) => {
+  return <></>;
+};
+
+export default HazardChart;
