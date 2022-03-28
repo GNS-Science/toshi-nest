19f053c847427c8b6127837cf1ccb688a59d2f68
QY <linqianye@tutanota.com>
2022-03-29 12:01:07 +1300

    delete component-lib

--- documentation/src/component-lib/HazardView.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-interface HazardViewProps {
-    id: string;
-}
-declare const HazardView: React.FC<HazardViewProps>;
-export default HazardView;
--- documentation/src/component-lib/charts/HazardChart.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-interface HazardChartProps {
-    id: string;
-    height: number;
-    width: number;
-    location: string;
-}
-declare const HazardChart: React.FC<HazardChartProps>;
-export default HazardChart;
