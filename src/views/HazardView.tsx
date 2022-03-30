43b1e2c94eef165ea8aec583747ac88f49da5b34
QY <linqianye@tutanota.com>
2022-03-31 10:01:05 +1300

    add hazard view component

---
+++ src/views/HazardView.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+import ControlsBar from "../common/ControlsBar";
+import SelectControl from "../common/SelectControl";
+
+interface HazardViewOptions {
+  locations: string[];
+  PGA: string[];
+  forecastTimes: string[];
+  gmpe: string[];
+  backgroundSeismicity: string[];
+}
+
+interface HazardViewProps {
+  id: string;
+  options: HazardViewOptions;
+}
+
+const HazardView: React.FC<HazardViewProps> = ({
+  id,
+  options,
+}: HazardViewProps) => {
+  const [location, setLocation] = useState<string>(options.locations[0]);
+  const [PGA, setPGA] = useState<string[]>([options.PGA[0]]);
+  const [forecastTime, setForecastTime] = useState<string>(
+    options.forecastTimes[0]
+  );
+  const [backgroundSeismicity, setBackgroundSeismicity] = useState<string>(
+    options.backgroundSeismicity[0]
+  );
+  const [POE, setPOE] = useState<string>("None");
+  const [gmpe, setGmpe] = useState<string>(options.gmpe[0]);
+
+  return (
+    <>
+      <p>hazard view</p>
+      <ControlsBar>
+        <SelectControl
+          name="Location"
+          options={options.locations}
+          setSelection={setLocation}
+        />
+        {/* need multiSelectHere for PGA */}
+        <SelectControl
+          name="Forecast Timespan"
+          options={options.forecastTimes}
+          setSelection={setForecastTime}
+        />
+        <SelectControl
+          name="Background Seismicity"
+          options={options.backgroundSeismicity}
+          setSelection={setBackgroundSeismicity}
+        />
+        <SelectControl
+          name="Ground Motion Model"
+          options={options.gmpe}
+          setSelection={setGmpe}
+        />
+        <SelectControl
+          name="Probability of Exceedence"
+          options={["None", "2%", "10%"]}
+          setSelection={setPOE}
+        />
+      </ControlsBar>
+    </>
+  );
+};
+
+export default HazardView;
