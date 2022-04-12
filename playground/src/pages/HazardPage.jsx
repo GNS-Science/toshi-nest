8f1821c9545ca6efb3e399fb149201827ed1261c
QY <linqianye@tutanota.com>
2022-04-13 10:44:43 +1200

    add demo

--- playground/src/pages/HazardPage.jsx
+++ playground/src/pages/HazardPage.jsx
@@ -74,25 +74,36 @@ export const filterData = (data, location, pgaValue, forecastTime, gmpe, backgro
 
 const HazardPage = () => {
   const hazardPageOption = getHazardTableOptions(hazardChartsData);
-  const colors = ['#000000', '#FE1100', '#73d629', '#ffd700', '#7fe5f0', '#003366', '#ff7f50', '#047806', '#4ca3dd'];
+  const colorsArray = ['#000000', '#FE1100', '#73d629', '#ffd700', '#7fe5f0', '#003366', '#ff7f50', '#047806', '#4ca3dd'];
 
-  const curves = filterMultipleCurves(['PGA'], hazardChartsData, hazardPageOption.locations[0], hazardPageOption.forecastTimes[0], hazardPageOption.gmpe[0], hazardPageOption.backgroundSeismicity[0]);
-  console.log(curves);
+  const curves = filterMultipleCurves(
+    ['PGA', '0.1'],
+    hazardChartsData,
+    hazardPageOption.locations[0],
+    hazardPageOption.forecastTimes[0],
+    hazardPageOption.gmpe[0],
+    hazardPageOption.backgroundSeismicity[0],
+  );
 
   const scalesConfig = {
     x: { type: 'log', domain: [1e-3, 10] },
     y: { type: 'log', domain: [1e-5, 1] },
   };
 
+  const colors = {
+    PGA: '#000000',
+    0.1: '#FE1100',
+  };
+
   return (
     <>
       <p>Hazard Page</p>
       <div style={{ border: 'solid black 1px', width: '100vw' }}>
-        <HazardCurves curves={curves} width={500} scalesConfig={scalesConfig} colors={colors} heading={'Static Hazard Curves'} subHeading={'subHeading'} />
+        <HazardCurves curves={curves} width={500} scalesConfig={scalesConfig} colors={colors} heading={'Static Hazard Curves'} subHeading={'subHeading'} gridNumTicks={5} />
       </div>
       <div style={{ border: 'solid black 1px', width: '100vw', display: 'flex', justifyContent: 'center' }}>
         <div style={{ width: '50vw', display: 'flex', justifyContent: 'center' }}>
-          <ResponsiveHazardCurves curves={curves} scalesConfig={scalesConfig} colors={colors} heading={'Responsive Hazard Curves'} subHeading={'subHeading'} />
+          <ResponsiveHazardCurves curves={curves} scalesConfig={scalesConfig} colors={colors} heading={'Responsive Hazard Curves'} subHeading={'subHeading'} gridNumTicks={5} />
         </div>
       </div>
     </>
