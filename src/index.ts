34fc33fe54535d023d7c88ccca2da3ecf4e807f5
benjamineac <ben.albrecht.chamberlain@gmail.com>
2022-05-13 16:17:31 +1200

    adds leaflet & fullscreen;

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ import HazardCurves from './charts/HazardCurves';
 import ResponsiveHazardCurves from './charts/ResponsiveHazardCurves';
 import { default as SpectralAccelerationChart } from './SpectralAccelerationChart';
 import { default as SpectralAccelerationChartResponsive } from './SpectralAccelerationChartResponsive';
+import LeafletMap from './charts/LeafletMap';
 
-export { ControlsBar, SelectControl, MultiSelect, HazardCurves, ResponsiveHazardCurves, SpectralAccelerationChart, SpectralAccelerationChartResponsive };
+export { ControlsBar, SelectControl, MultiSelect, HazardCurves, ResponsiveHazardCurves, SpectralAccelerationChart, SpectralAccelerationChartResponsive, LeafletMap };
