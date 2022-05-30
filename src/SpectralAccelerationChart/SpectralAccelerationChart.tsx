13c129db9c6d48b054ae5fa4d4b3185be7118993
QY <linqianye@tutanota.com>
2022-05-30 14:04:11 +1200

    consistent plot heading component

--- src/SpectralAccelerationChart/SpectralAccelerationChart.tsx
+++ src/SpectralAccelerationChart/SpectralAccelerationChart.tsx
@@ -1,36 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Typography } from '@mui/material';
 import { curveLinear } from '@visx/curve';
 import { AnimatedAxis, AnimatedLineSeries, Grid, Tooltip, XYChart } from '@visx/xychart';
 
 import { SpectralAccelerationChartProps } from './spectralAccelerationChart.types';
 import { XY } from '../types/common.types';
+import PlotHeadings from '../common/PlotHeadings';
 
 const SpectralAccelerationChart: React.FC<SpectralAccelerationChartProps> = ({ data, heading, subHeading, width }: SpectralAccelerationChartProps) => {
-  const [headingSize, setHeadingSize] = useState<number>(0);
-  const [subHeadingSize, setSubHeadingSize] = useState<number>(0);
-
-  const headingProps = {
-    alignmnetbaseline: 'middle',
-    dominantBaseline: 'middle',
-    textAnchor: 'middle',
-  };
-
-  useEffect(() => {
-    width * 0.035 >= 24 ? setHeadingSize(24) : setHeadingSize(width * 0.035);
-    width * 0.025 >= 15 ? setSubHeadingSize(15) : setSubHeadingSize(width * 0.025);
-  }, [width]);
-
   return (
     <>
       <div style={{ position: 'relative', width: width }}>
         <XYChart height={width * 0.75} width={width} xScale={{ type: 'linear', domain: [-1, 10] }} yScale={{ type: 'linear', domain: [0, 3] }}>
-          <text y={18} x={'50%'} {...headingProps} fontSize={headingSize} fontWeight="bold">
-            {heading}
-          </text>
-          <text y={headingSize + 18} x={'50%'} {...headingProps} fontSize={subHeadingSize} style={{ margin: 10 }}>
-            {subHeading}
-          </text>
+          <PlotHeadings heading={heading} subHeading={subHeading} width={width} />
           <AnimatedAxis label="Spectral Period (s)" orientation="bottom" />
           <AnimatedAxis label="Pseudo-Spectral Acceleration (g)" orientation="left" />
           <Tooltip
