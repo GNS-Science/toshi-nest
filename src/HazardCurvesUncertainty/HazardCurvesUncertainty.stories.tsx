737239c33b81b936af39447a1fcf65a15e871718
0qy <linqianye@tutanota.com>
2022-07-04 11:15:48 +1200

    hazard uncertainty component and story setup

---
+++ src/HazardCurvesUncertainty/HazardCurvesUncertainty.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import HazardCurvesUncertianty from './HazardCurvesUncertainty';
+
+export default {
+  title: 'Charts/HazardCurvesUncertainty',
+  component: HazardCurvesUncertianty,
+} as ComponentMeta<typeof HazardCurvesUncertianty>;
+
+const Template: ComponentStory<typeof HazardCurvesUncertianty> = (args) => <HazardCurvesUncertianty {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  id: '',
+};
---
+++ src/HazardCurvesUncertainty/HazardCurvesUncertainty.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export interface HazardCurvesUncertaintyProps {
+  id: string;
+}
+
+const HazardCurvesUncertianty: React.FC<HazardCurvesUncertaintyProps> = (props: HazardCurvesUncertaintyProps) => {
+  return (
+    <>
+      <p>thingy</p>
+    </>
+  );
+};
+
+export default HazardCurvesUncertianty;
