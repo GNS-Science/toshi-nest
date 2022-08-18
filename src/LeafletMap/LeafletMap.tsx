0ca375f9aeda70c296cbcfee19840a30dc960390
QY <linqianye@tutanota.com>
2022-08-19 09:14:21 +1200

    map renders geojson according to feature styles

--- src/LeafletMap/LeafletMap.tsx
+++ src/LeafletMap/LeafletMap.tsx
@@ -6,12 +6,6 @@ import { LeafletMapProps } from './LeafletMap.types';
 import { MapContainer, TileLayer, GeoJSON, LayersControl } from 'react-leaflet';
 import Fullscreen from 'react-leaflet-fullscreen-plugin';
 
-const mapStyle = {
-  color: '#f21616',
-  weight: 1,
-  opacity: 0.65,
-};
-
 const { BaseLayer } = LayersControl;
 
 const LeafletMap: React.FC<LeafletMapProps> = (props: LeafletMapProps) => {
@@ -41,7 +35,21 @@ const LeafletMap: React.FC<LeafletMapProps> = (props: LeafletMapProps) => {
         </LayersControl>
         {geoJsonData.length &&
           geoJsonData.map((data, index) => {
-            return <GeoJSON key={`geojson layer ${index}`} data={parseGeoJson(data)} style={mapStyle} />;
+            return (
+              <GeoJSON
+                key={`geojson layer ${index}`}
+                data={parseGeoJson(data)}
+                style={(feature) => {
+                  return {
+                    stroke: feature?.properties.stroke,
+                    color: feature?.properties.fill,
+                    weight: feature?.properties['stroke-width'],
+                    opacity: feature?.properties['stroke-opacity'],
+                    fillOpacity: feature?.properties['fill-opacity'],
+                  };
+                }}
+              />
+            );
           })}
         <Fullscreen
           eventHandlers={{
