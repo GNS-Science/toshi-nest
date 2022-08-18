296957affbd9c701868eb752dacde69df14fb6b3
QY <linqianye@tutanota.com>
2022-08-19 09:14:34 +1200

    add story

--- src/LeafletMap/LeafletMap.stories.tsx
+++ src/LeafletMap/LeafletMap.stories.tsx
@@ -5,6 +5,7 @@ import { ComponentMeta } from '@storybook/react';
 import LeafletMap from './LeafletMap';
 import { solvisResponse } from './LeafletMapTestData';
 import { LatLngExpression } from 'leaflet';
+import hazardMapsGeojsonTestData from '../__tests__/testData/hazardMapsGeojsonTestData';
 
 export default {
   title: 'Controls/LeafletMap',
@@ -20,16 +21,14 @@ export const Primary = () => {
   const zoom = 5;
   const nzCentre = [-40.946, 174.167];
 
-  return (
-    <LeafletMap
-      zoom={zoom}
-      nzCentre={nzCentre as LatLngExpression}
-      geoJsonData={geoJsonData}
-      // rupturesData={rupturesData}
-      // locationsData={locationsData}
-      height={'700px'}
-      width={'100%'}
-      setFullscreen={setFullscreen}
-    />
-  );
+  return <LeafletMap zoom={zoom} nzCentre={nzCentre as LatLngExpression} geoJsonData={geoJsonData} height={'700px'} width={'100%'} setFullscreen={setFullscreen} />;
+};
+
+export const HazardMaps = () => {
+  const [fullscreen, setFullscreen] = useState<boolean>(false);
+
+  const zoom = 5;
+  const nzCentre = [-40.946, 174.167];
+
+  return <LeafletMap zoom={zoom} nzCentre={nzCentre as LatLngExpression} geoJsonData={hazardMapsGeojsonTestData} height={'700px'} width={'100%'} setFullscreen={setFullscreen} />;
 };
