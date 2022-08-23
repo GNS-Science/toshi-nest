e7b49ea1b63a0782ab6e06c8ccd701b5bdeaeda8
QY <linqianye@tutanota.com>
2022-08-23 15:31:57 +1200

    add leaflet story

--- src/LeafletMap/LeafletMap.stories.tsx
+++ src/LeafletMap/LeafletMap.stories.tsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
-
+import React, { useState, useMemo, useEffect } from 'react';
 import { ComponentMeta } from '@storybook/react';
-
-import LeafletMap from './LeafletMap';
-import { solvisResponse } from './LeafletMapTestData';
+import { GeoJsonObject } from 'geojson';
+import { Box } from '@mui/material';
 import { LatLngExpression } from 'leaflet';
-import hazardMapsGeojsonTestData from '../__tests__/testData/hazardMapsGeojsonTestData';
+
+import SelectControl from '../SelectControl';
+import LeafletMap from './LeafletMap';
+import LeafletDrawer from '../LeafletDrawer';
+import { solvisResponse } from './LeafletMapTestData';
+import geojsonTesetDat05 from '../__tests__/testData/geoJson/geojsonTestData0.5';
+import geojsonTestDataPGA from '../__tests__/testData/geoJson/geojsonTestDataPGA';
 
 export default {
   title: 'Controls/LeafletMap',
   component: LeafletMap,
+  subcomponents: { LeafletDrawer, SelectControl },
 } as ComponentMeta<typeof LeafletMap>;
 
 export const Primary = () => {
@@ -46,5 +51,37 @@ export const HazardMaps = () => {
   const zoom = 5;
   const nzCentre = [-40.946, 174.167];
 
-  return <LeafletMap zoom={zoom} nzCentre={nzCentre as LatLngExpression} geoJsonData={hazardMapsGeojsonTestData} height={'700px'} width={'100%'} setFullscreen={setFullscreen} />;
+  return <LeafletMap zoom={zoom} nzCentre={nzCentre as LatLngExpression} geoJsonData={geojsonTesetDat05} height={'700px'} width={'100%'} setFullscreen={setFullscreen} />;
+};
+
+export const HazardMapsWithControls = () => {
+  const [fullscreen, setFullscreen] = useState<boolean>(false);
+  const [selection, setSelection] = useState<string>('option1(PGA)');
+
+  const geoJson = useMemo<string[]>(() => {
+    switch (selection) {
+      case 'option1(PGA)':
+        return geojsonTestDataPGA;
+
+      case 'option2(SA0.5)':
+        return geojsonTesetDat05;
+
+      default:
+        return [];
+    }
+  }, [selection]);
+
+  const zoom = 5;
+  const nzCentre = [-40.946, 174.167];
+
+  return (
+    <>
+      <LeafletMap zoom={zoom} geoJsonData={geoJson} nzCentre={nzCentre as LatLngExpression} height={'700px'} width={'100%'} setFullscreen={setFullscreen} />;
+      <LeafletDrawer drawerHeight={'700px'} headerHeight={'0px'} width={'400px'} fullscreen={fullscreen}>
+        <Box sx={{ width: '100%', margin: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+          <SelectControl options={['option1(PGA)', 'option2(SA0.5)', 'option3(null)']} selection={selection} setSelection={setSelection} name="Options" />
+        </Box>
+      </LeafletDrawer>
+    </>
+  );
 };
