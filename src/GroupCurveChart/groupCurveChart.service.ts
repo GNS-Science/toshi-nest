eecea39d69f56a7055a73189093514c89c58a17c
ben-chamberlain <ben.albrecht.chamberlain@gmail.com>
2022-09-15 10:16:57 +1200

    linear spectral chart doesnt remove 0 points from uncertainty area; (#106)

--- src/GroupCurveChart/groupCurveChart.service.ts
+++ src/GroupCurveChart/groupCurveChart.service.ts
@@ -1,6 +1,6 @@
 import { CurveGroup, Datum } from './groupCurveChart.types';
 
-export const getAreaData = (curveGroup: CurveGroup) => {
+export const getAreaData = (curveGroup: CurveGroup, scaleType: string) => {
   const area: number[][] = [];
 
   curveGroup['lower1'].data.map((point, index) => {
@@ -8,7 +8,7 @@ export const getAreaData = (curveGroup: CurveGroup) => {
     areaPoint.push(point[0]);
     areaPoint.push(point[1]);
     areaPoint.push(curveGroup['upper1'].data[index][1]);
-    !areaPoint.includes(0) && area.push(areaPoint);
+    scaleType === 'log' ? !areaPoint.includes(0) && area.push(areaPoint) : area.push(areaPoint);
   });
 
   return area;
