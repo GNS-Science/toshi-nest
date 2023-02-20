575b64e0f98bafd17916f4cb53141306188032c1
ben-chamberlain <ben.albrecht.chamberlain@gmail.com>
2023-02-20 17:14:03 +1300

    add rollup css plugin;

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from 'rollup-plugin-typescript2';
 import del from 'rollup-plugin-delete';
+import css from 'rollup-plugin-css-only';
 import pkg from './package.json';
 
 export default [
@@ -32,6 +33,7 @@ export default [
           exclude: ['**/__tests__', '**/*.test.ts', '*/setupTests.ts', '**/*.test.tsx', '**/*.stories.tsx', '**/*TestData.ts'],
         },
       }),
+      css({ output: 'public/build/extra.css' }),
     ],
     external: [Object.keys(pkg.peerDependencies || {}), 'node_modules/leaflet/dist/leaflet.css', 'node_modules/leaflet-timedimension/src/leaflet.timedimension.control.css'],
   },
