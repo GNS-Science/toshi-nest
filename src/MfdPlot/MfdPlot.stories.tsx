a262ab1835fd0db06a4d6037f77c058369bc6e4d
ben-chamberlain <ben.albrecht.chamberlain@gmail.com>
2024-05-28 13:04:31 +1200

    remove console.logs;

--- src/MfdPlot/MfdPlot.stories.tsx
+++ src/MfdPlot/MfdPlot.stories.tsx
@@ -153,7 +153,6 @@ const data = [
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const renderCustomTooltip = (tooltipData: any) => {
   const datum = tooltipData?.nearestDatum?.datum as Datum;
-  console.log(tooltipData);
   return (
     <>
       <Typography>Rate: {datum?.rate?.toExponential(2)}</Typography>
