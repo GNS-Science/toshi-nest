44343307b39c08519dbfc97aaf8447e51f728b96
Oakley Jurgens <o.jurgens@gns.cri.nz>
2026-08-27 13:16:43 +1200

    [175] fix default basemap

--- src/LeafletMap/LeafletLayers.tsx
+++ src/LeafletMap/LeafletLayers.tsx
@@ -15,16 +15,12 @@ const { BaseLayer } = LayersControl;
 const BaseLayerOptions: React.FC = () => {
   return (
     <>
-      <BaseLayer name="CartoDB: VoyagerLabelsUnder" checked={true}>
-        <LayerGroup>
-          <TileLayer
-            url={'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png'}
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            maxZoom={20}
-          />
-        </LayerGroup>
+      <BaseLayer name="OpenStreetMap" checked={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
       </BaseLayer>
-
       <BaseLayer name="Esri: WorldGrayCanvas">
         <TileLayer
           url={'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'}
@@ -44,7 +40,7 @@ const BaseLayerOptions: React.FC = () => {
         <TileLayer
           url={'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'}
           attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
-          // maxZoom={13}
+        // maxZoom={13}
         />
       </BaseLayer>
     </>
@@ -58,35 +54,35 @@ const BaseLayerOverlayOptions: React.FC = () => {
         <TileLayer
           url={'https://maps.scinfo.org.nz/mapcache/pwms/tms/1.0.0/text@GoogleMapsCompatible/{z}/{x}/{y}.png'}
           tms={true}
-          // attribution="&copy; Landcare Research NZ Limited 2009-2022. Contains data sourced from LINZ. Crown Copyright Reserved."
+        // attribution="&copy; Landcare Research NZ Limited 2009-2022. Contains data sourced from LINZ. Crown Copyright Reserved."
         />
       </LayersControl.Overlay>
       <LayersControl.Overlay name="Transport">
         <TileLayer
           url={'https://maps.scinfo.org.nz/mapcache/pwms/tms/1.0.0/transport@g/{z}/{x}/{y}.png'}
           tms={true}
-          // attribution="&copy; Landcare Research NZ Limited 2009-2022. Contains data sourced from LINZ. Crown Copyright Reserved."
+        // attribution="&copy; Landcare Research NZ Limited 2009-2022. Contains data sourced from LINZ. Crown Copyright Reserved."
         />
       </LayersControl.Overlay>
       <LayersControl.Overlay name="Water">
         <TileLayer
           url={'https://maps.scinfo.org.nz/mapcache/pwms/tms/1.0.0/water@g/{z}/{x}/{y}.png'}
           tms={true}
-          // attribution="&copy; Landcare Research NZ Limited 2009-2022. Contains data sourced from LINZ. Crown Copyright Reserved."
+        // attribution="&copy; Landcare Research NZ Limited 2009-2022. Contains data sourced from LINZ. Crown Copyright Reserved."
         />
       </LayersControl.Overlay>
       <LayersControl.Overlay name="Contours">
         <TileLayer
           url={'https://maps.scinfo.org.nz/mapcache/pwms/tms/1.0.0/contours@g/{z}/{x}/{y}.png'}
           tms={true}
-          // attribution="&copy; Landcare Research NZ Limited 2009-2022. Contains data sourced from LINZ. Crown Copyright Reserved."
+        // attribution="&copy; Landcare Research NZ Limited 2009-2022. Contains data sourced from LINZ. Crown Copyright Reserved."
         />
       </LayersControl.Overlay>
       <LayersControl.Overlay name="Urban">
         <TileLayer
           url={'https://maps.scinfo.org.nz/mapcache/pwms/tms/1.0.0/urban@g/{z}/{x}/{y}.png'}
           tms={true}
-          // attribution="&copy; Landcare Research NZ Limited 2009-2022. Contains data sourced from LINZ. Crown Copyright Reserved."
+        // attribution="&copy; Landcare Research NZ Limited 2009-2022. Contains data sourced from LINZ. Crown Copyright Reserved."
         />
       </LayersControl.Overlay>
     </Pane>
